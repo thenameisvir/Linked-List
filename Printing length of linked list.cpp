62335493185ad9777ae4d935883e5d526f0cf3a7
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-01-31 22:25:07 +0530

    Add files via upload

---
+++ Printing length of linked list.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node* next;
+
+  Node(){
+        this->next = NULL;
+  }
+
+  Node(int data){
+    this->data = data;
+    this->next = NULL;
+  }
+};
+
+void print(Node* head){
+  Node* temp = head;
+  int c = 0;
+  while(temp!=NULL){
+    // cout<<temp->data<<"->";
+    temp = temp->next;
+    c++;
+  }
+  cout<<c<<endl;
+
+
+}
+int main() {
+  
+ Node* first = new Node(33);
+ Node* second = new Node(13);
+ Node* third = new Node(23);
+ Node* fourth = new Node(63);
+ Node* fifth = new Node(83);
+
+ first->next = second;  
+ second->next = third;
+ third->next = fourth;
+ fourth->next = fifth;
+
+ Node* head = first;
+ print(head);
+
+
+
+  return 0;
+}
