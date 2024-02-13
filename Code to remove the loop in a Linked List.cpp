487935184494e49fe2c0c6fcf2f06c09948d5856
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-02-14 00:15:51 +0530

    Add files via upload

---
+++ Code to remove the loop in a Linked List.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node* next;
+
+  Node(int data){
+    this->data = data;
+    this->next = NULL;
+  }
+};
+void insertathead(Node* &head,Node* &tail,int data){
+  if(head==NULL){
+    Node* newNode = new Node(data);
+    head = newNode;
+    tail = newNode;
+    return;
+  }
+  Node* newNode = new Node(data);
+  newNode->next = head;
+  head = newNode;
+}
+void insertattail(Node* &head,Node* &tail,int data){
+  if(head==NULL){
+    Node* newNode = new Node(data);
+    head = newNode;
+    tail = newNode;
+    return;
+  }
+  Node* newNode = new Node(data);
+  tail->next = newNode;
+  tail = newNode;
+}
+
+Node* getstartingpointofLoop(Node* &head){
+  Node* slow = head;
+  Node* fast = head;
+  while(fast!=NULL&&fast->next!=NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if(fast==slow){
+      break;
+    }
+  }
+
+  slow = head;
+  while(slow!=fast){
+    slow = slow->next;
+    fast = fast->next;
+  }
+
+  return slow;
+
+}
+void remove(Node* &head){
+  Node* slow = head;
+  Node* fast = head;
+  while(fast!=NULL&&fast->next!=NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if(fast==slow){
+      break;
+    }
+  }
+
+  slow = head;
+  while(slow!=fast){
+    slow = slow->next;
+    fast = fast->next;
+  }
+
+  Node* starting = slow;
+  Node* temp = fast;
+  while(temp->next!=slow){
+    temp = temp->next;
+  }
+
+  temp->next = NULL;
+}
+void print(Node* &head){
+  Node* temp = head;
+  while(temp!=NULL){
+    cout<<temp->data<<" ";
+    temp = temp->next;
+  }
+}
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    // Insert nodes to create a linked list with a loop
+    head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    // Create a loop from the last node to the second node
+    head->next->next->next->next->next = head->next;
+
+    // Detect and remove the loop
+    Node* startingPoint = getstartingpointofLoop(head);
+    cout << "Starting point of the loop: " << startingPoint->data << endl;
+    remove(head);
+    
+    // Print the linked list after removing the loop
+    cout << "Linked List after removing the loop: ";
+    print(head);
+    cout << endl;
+
+    return 0;
+}
+
